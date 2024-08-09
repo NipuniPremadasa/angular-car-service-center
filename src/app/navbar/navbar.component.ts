1426b642525213f7e86f0c455c7002cd45ba7a7f
Nipuni Premadasa <nipunipremadasa923@gmail.com>
2024-08-09 13:41:56 +0530

    Edit navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,26 +1,59 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: []
 })
-export class NavbarComponent {
-  dropdownOpen = false; // State to manage the dropdown visibility
+export class NavbarComponent implements OnInit, OnDestroy {
+  isLanguageDropdownOpen = false;
+  isMobileMenuOpen = false;
+  currentRoute: string = '';
+  private clickListener: (() => void) | undefined;
 
-  // Method to toggle the dropdown visibility
-  toggleDropdown(): void {
-    this.dropdownOpen = !this.dropdownOpen;
+  constructor(private renderer: Renderer2, private elementRef: ElementRef, private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
   }
 
-  // Method to close the dropdown when clicking outside
-  @HostListener('document:click', ['$event'])
-  clickOut(event: MouseEvent): void {
-    // Close the dropdown if it's open and the click is outside the dropdown
-    if (this.dropdownOpen && !(event.target as HTMLElement).closest('.dropdown-toggle')) {
-      this.dropdownOpen = false;
+  ngOnInit() {
+    this.clickListener = this.renderer.listen('document', 'click', (event: Event) => {
+      if (!this.elementRef.nativeElement.contains(event.target)) {
+        this.isLanguageDropdownOpen = false;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.clickListener) {
+      this.clickListener();
     }
   }
+
+  toggleLanguageDropdown(event: Event) {
+    event.stopPropagation();
+    this.isLanguageDropdownOpen = !this.isLanguageDropdownOpen;
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+    // // Method to set the current route
+    setCurrentRoute(route: string) {
+      this.currentRoute = route;
+    }
+  
+    // Method to check if a route is active
+    isActive(route: string): boolean {
+      return this.currentRoute === route;
+    }
 }
